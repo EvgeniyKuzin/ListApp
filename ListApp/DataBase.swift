659859ed0f2541d9f9093a02e890ee26d9dee750
EvgeniyKuzin <vaglover@yandex.ru>
2024-01-30 23:44:59 +0300

    UI is ready (list and details view)

---
+++ ListApp/DataBase.swift
@@ -0,0 +1,8 @@
+//
+//  DataBase.swift
+//  ListApp
+//
+//  Created by Evgeniy Kuzin on 30.01.2024.
+//
+
+import Foundation
---
+++ ListApp/TrackDetailsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TrackDetailsViewController.swift
+//  ListApp
+//
+//  Created by Evgeniy Kuzin on 30.01.2024.
+//
+
+import UIKit
+
+class TrackDetailsViewController: UIViewController {
+
+    @IBOutlet var artCoverImageView: UIImageView!
+    
+    
+    @IBOutlet var trackLabel: UILabel!
+    
+    @IBOutlet var albumLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
